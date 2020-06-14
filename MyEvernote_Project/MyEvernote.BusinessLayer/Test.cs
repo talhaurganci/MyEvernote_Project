d8a5e0c5206970a015bccf21407cac91e401e7f0
talhaurganci <talhaurganci@gmail.com>
2020-06-14 18:32:04 +0300

    MyEverNote_Project
    Web_Project

---
+++ MyEvernote_Project/MyEvernote.BusinessLayer/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEvernote.BusinessLayer
+{
+    public class Test
+    {
+        public Test()
+        {
+            DataAccessLayer.EntityFramework.DatabaseContext db = new DataAccessLayer.EntityFramework.DatabaseContext();
+            db.Categories.ToList();
+        }
+           
+        
+    }
+}
